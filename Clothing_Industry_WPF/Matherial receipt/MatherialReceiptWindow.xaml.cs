15767ac91760f2636d0541d3ded9a1c5eaf38bb5
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2019-04-28 13:15:32 +0300

    Revert "1"
    This reverts commit 6c91c6ee5ebd57df3783737e717785f76eb89187.

---
+++ Clothing_Industry_WPF/Matherial receipt/MatherialReceiptWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clothing_Industry_WPF.Matherial_receipt
+{
+    /// <summary>
+    /// Логика взаимодействия для MatherialReceiptWindow.xaml
+    /// </summary>
+    public partial class MatherialReceiptWindow : Window
+    {
+        public MatherialReceiptWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
