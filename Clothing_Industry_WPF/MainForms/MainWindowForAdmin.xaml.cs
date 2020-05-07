436831896c12f7f75a9404d35d9b4cfe8e0be4dd
lexus <lexus@DESKTOP-09I91RH>
2020-05-07 13:17:59 +0300

    1.8

---
+++ Clothing_Industry_WPF/MainForms/MainWindowForAdmin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clothing_Industry_WPF.MainForms
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowExperimental.xaml
+    /// </summary>
+    public partial class WindowExperimental : Window
+    {
+        public WindowExperimental()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+    }
+}
